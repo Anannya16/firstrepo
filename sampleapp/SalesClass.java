241d26e75a8e6901a238af3cde90c8eb41ee260e
anamahap <anamahap@LIN25005211.corp.capgemini.com>
2021-08-16 16:09:00 +0530

    Commit All

---
+++ sampleapp/SalesClass.java
@@ -0,0 +1,16 @@
+package sampleapp;
+
+public class SalesClass {
+
+	public static void display() {
+		int[] arr= {23,45,56,78,6};
+		for(int num:arr)
+			System.out.println(num);
+		
+	}
+	public void print() {
+		System.out.println("Print All Array Number:");
+		display();
+	}
+
+}
