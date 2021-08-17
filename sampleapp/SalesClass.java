bb45d01c39d84a095924e6a6b0d3f5953317b6b2
anamahap <anamahap@mumd22107.corp.capgemini.com>
2021-08-17 11:55:28 +0530

    Display method updated in sales class.

--- sampleapp/SalesClass.java
+++ sampleapp/SalesClass.java
@@ -2,12 +2,14 @@ package sampleapp;
 
 public class SalesClass {
 
-	public static void display() {
+	public void display() {
 		int[] arr= {23,45,56,78,6};
-		for(int num:arr)
-			System.out.println(num);
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
 		
+		System.out.println("Array Size:" + arr.length);		
 	}
+	
 	public void print() {
 		System.out.println("Print All Array Number:");
 		display();
